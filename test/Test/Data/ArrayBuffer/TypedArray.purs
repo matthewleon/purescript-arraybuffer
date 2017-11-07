c05eaa96e788c53440e02a68de2444baaf747c03
Matthew Leon <ml@matthewleon.com>
2017-11-07 14:36:06 -0500

    kill node buffer dependency

--- test/Test/Data/ArrayBuffer/TypedArray.purs
+++ test/Test/Data/ArrayBuffer/TypedArray.purs
@@ -4,18 +4,16 @@ import Prelude
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log, logShow)
-import Control.Monad.Eff.Unsafe (unsafePerformEff)
 import Data.ArrayBuffer.TypedArray as TA
 import Data.Maybe (Maybe)
-import Node.Buffer as NB
 
 testTypedArray :: forall e. Eff ( console :: CONSOLE | e ) Unit
 testTypedArray =
   let arr = [100, 101, 102, 103]
-      nb  = unsafeArrayToNodeBuffer arr
-      ab  = unsafePerformEff $ NB.toArrayBuffer nb
+      i8a :: TA.Int8Array
+      i8a = TA.fromArray arr
+      ab  = TA.buffer i8a
   in do
-    logShow nb
     log $ TA.show (TA.fromArrayBuffer ab :: TA.Int8Array)
     log $ TA.show (TA.fromArrayBuffer ab :: TA.Int16Array)
     log $ TA.show (TA.fromArrayBuffer ab :: TA.Int32Array)
@@ -23,6 +21,3 @@ testTypedArray =
     logShow $ TA.show <$> (TA.fromArrayBufferWithOffsetAndLength ab 2 2 :: Maybe TA.Int8Array)
     logShow $ TA.show <$> (TA.fromArrayBufferWithOffsetAndLength ab 2 3 :: Maybe TA.Int8Array)
     logShow $ TA.show <$> (TA.fromArrayBufferWithOffsetAndLength ab 3 3 :: Maybe TA.Int8Array)
-
-unsafeArrayToNodeBuffer :: Array NB.Octet -> NB.Buffer
-unsafeArrayToNodeBuffer = unsafePerformEff <<< NB.fromArray
