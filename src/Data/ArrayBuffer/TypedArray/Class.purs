de043971b06ec73956e00f1760e8e0856ff6f590
Matthew Leon <ml@matthewleon.com>
2017-11-07 12:36:52 -0500

    reintroduce UInts

--- src/Data/ArrayBuffer/TypedArray/Class.purs
+++ src/Data/ArrayBuffer/TypedArray/Class.purs
@@ -5,6 +5,7 @@ module Data.ArrayBuffer.TypedArray.Class (
 ) where
 
 import Data.ArrayBuffer.Types (ArrayView, Int8Array, Uint8Array, Int16Array, Uint16Array, Int32Array, Uint32Array, Float32Array, Int8, Uint8, Int16, Uint16, Int32, Uint32, Float32)
+import Data.UInt (UInt)
 
 class IsArrayType t m | t -> m where
   constructor :: Constructor t
@@ -13,15 +14,15 @@ data Constructor t
 
 instance arrayTypeInt8Array :: IsArrayType (ArrayView Int8) Int
   where constructor = int8ArrayConstructor
-instance arrayTypeUint8Array :: IsArrayType (ArrayView Uint8) Int
+instance arrayTypeUint8Array :: IsArrayType (ArrayView Uint8) UInt
   where constructor = uint8ArrayConstructor
 instance arrayTypeInt16Array :: IsArrayType (ArrayView Int16) Int
   where constructor = int16ArrayConstructor
-instance arrayTypeUint16Array :: IsArrayType (ArrayView Uint16) Int
+instance arrayTypeUint16Array :: IsArrayType (ArrayView Uint16) UInt
   where constructor = uint16ArrayConstructor
 instance arrayTypeInt32Array :: IsArrayType (ArrayView Int32) Int
   where constructor = int32ArrayConstructor
-instance arrayTypeUint32Array :: IsArrayType (ArrayView Uint32) Int
+instance arrayTypeUint32Array :: IsArrayType (ArrayView Uint32) UInt
   where constructor = uint32ArrayConstructor
 instance arrayTypeFloat32Array :: IsArrayType (ArrayView Float32) Number
   where constructor = float32ArrayConstructor
