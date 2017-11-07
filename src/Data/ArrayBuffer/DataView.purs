a3ac609a018e6a4334260060dfcb85206b12c4e5
Matthew Leon <ml@matthewleon.com>
2017-11-07 14:56:04 -0500

    kill commented getTypedArray code

--- src/Data/ArrayBuffer/DataView.purs
+++ src/Data/ArrayBuffer/DataView.purs
@@ -18,8 +18,6 @@ module Data.ArrayBuffer.DataView (
 , getUint32le
 , getFloat32le
 , getFloat64le
---, getTypedArray
---, getTypedArrayWithLength
 ) where
 
 import Data.ArrayBuffer.Types (ArrayBuffer, ByteLength, ByteOffset, DataView)
@@ -94,10 +92,3 @@ getFloat64be = getter "getFloat64" 8 false
 
 getFloat64le :: Getter Number
 getFloat64le = getter "getFloat64" 8 true
-
-{-
-getTypedArray :: forall t. Getter (ArrayView t)
-getTyepdArray dv bo = buffer dv
-
-getTypedArrayWithLength :: forall t. Int -> Getter (ArrayView t)
--}
