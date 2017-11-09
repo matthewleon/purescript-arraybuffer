e67613ff5d1d419fde7dc9224fecea9e8b4f03a1
Matthew Leon <ml@matthewleon.com>
2017-11-09 16:08:11 -0500

    ST getInt16le tests

--- test/Test/Data/ArrayBuffer/DataView/ST.purs
+++ test/Test/Data/ArrayBuffer/DataView/ST.purs
@@ -41,3 +41,37 @@ testSTDataView = describe "STDataView" do
               Nothing == pureST (
                   flip STDV.getInt8 i =<< STDV.fromArrayBuffer (TA.buffer i8a)
               )
+
+  describe "getInt16le" $ do
+    it "correctly gets 16-bit integers" $
+      quickCheck \(TDV.NonEmptyUntypedInt16Array xs) ->
+        let i16a = (TA.fromArray xs) :: TA.Int16Array
+            index = chooseInt 0 (A.length xs - 1)
+        in  index <#> \i ->
+              let byteIndex = i * 2
+              in  isJust (A.index xs i)
+                  && A.index xs i == pureST (
+                       flip STDV.getInt16le byteIndex
+                       =<< STDV.fromArrayBuffer (TA.buffer i16a)
+                     )
+    it "returns Nothing for out of range index" $
+      quickCheck \(TDV.NonEmptyUntypedInt16Array xs) i ->
+        let i16a = (TA.fromArray xs) :: TA.Int16Array
+            index = A.length xs + i
+            byteIndex = index * 2
+        in  A.index xs index == Nothing &&
+            Nothing == pureST (
+                flip STDV.getInt16le byteIndex
+                =<< STDV.fromArrayBuffer (TA.buffer i16a)
+              )
+    it "returns Nothing for negative index" $
+      quickCheck \(TDV.NonEmptyUntypedInt16Array xs) ->
+        let i16a = (TA.fromArray xs) :: TA.Int16Array
+            index = (-1 - _) <$> chooseInt 0 (A.length xs - 1)
+        in  index <#> \i ->
+              let byteIndex = i * 2
+              in  A.index xs i == Nothing &&
+                  Nothing == pureST (
+                      flip STDV.getInt16le byteIndex
+                      =<< STDV.fromArrayBuffer (TA.buffer i16a)
+                  )
