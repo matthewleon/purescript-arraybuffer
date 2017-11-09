ec739383834008946cf235e12c5116e92f06c778
Matthew Leon <ml@matthewleon.com>
2017-11-09 14:47:30 -0500

    first STDataView test

---
+++ test/Test/Data/ArrayBuffer/DataView/ST.purs
@@ -0,0 +1,34 @@
+module Test.Data.ArrayBuffer.DataView.ST where
+
+import Prelude
+
+import Control.Monad.ST (pureST)
+import Data.Array as A
+import Data.ArrayBuffer.DataView.ST as STDV
+import Data.ArrayBuffer.TypedArray as TA
+import Data.Maybe (isJust)
+import Test.Data.ArrayBuffer.DataView as TDV
+import Test.QuickCheck.Gen (chooseInt)
+import Test.Spec (Spec, describe, it)
+import Test.Spec.QuickCheck (QCRunnerEffects, quickCheck)
+
+testSTDataView :: Spec (QCRunnerEffects ()) Unit
+testSTDataView = describe "STDataView" do
+  describe "getInt8" $ do
+    it "correctly gets 8-bit integers" $
+      quickCheck \(TDV.NonEmptyUntypedInt8Array xs) ->
+        let i8a = (TA.fromArray xs) :: TA.Int8Array
+            index = chooseInt 0 (A.length xs - 1)
+        in  index <#> \i ->
+              isJust (A.index xs i) &&
+              A.index xs i == pureST (
+                flip STDV.getInt8 i =<< STDV.fromArrayBuffer (TA.buffer i8a)
+              )
+              {-
+    it "returns Nothing for out of range index" $
+      quickCheck \(NonEmptyUntypedInt8Array xs) i ->
+        let i8a = (TA.fromArray xs) :: TA.Int8Array
+            index = A.length xs + i
+        in  A.index xs index == Nothing &&
+            DV.getInt8 (DV.fromArrayBuffer $ TA.buffer i8a) index == Nothing
+-}
--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Control.Monad.Eff (Eff)
 import Test.Data.ArrayBuffer.DataView (testDataView)
+import Test.Data.ArrayBuffer.DataView.ST (testSTDataView)
 import Test.Spec.QuickCheck (QCRunnerEffects)
 import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (run)
@@ -12,3 +13,4 @@ import Test.Spec.Runner (run)
 main :: Eff (QCRunnerEffects ()) Unit
 main = run [consoleReporter] do
   testDataView
+  testSTDataView
