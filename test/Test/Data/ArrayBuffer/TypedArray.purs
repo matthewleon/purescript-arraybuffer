95f886938f3b3fe7b3736f4031016fb82dda638d
Matthew Leon <ml@matthewleon.com>
2017-11-12 13:26:24 -0500

    test equality for other int array lengths

--- test/Test/Data/ArrayBuffer/TypedArray.purs
+++ test/Test/Data/ArrayBuffer/TypedArray.purs
@@ -11,18 +11,52 @@ import Test.Spec.QuickCheck (QCRunnerEffects, quickCheck)
 testTypedArray :: Spec (QCRunnerEffects ()) Unit
 testTypedArray = describe "TypedArray" do
   describe "eq" $ do
-    it "returns true for equal Int32Arrays" $
-      quickCheck \xs ->
-        TA.fromArray xs `TA.eq` (TA.fromArray xs :: TA.Int32Array)
-    it "returns false for unequal Int32Arrays" $
-      quickCheck \xs ->
-        arbitrary `suchThat` notEq xs <#> \ys ->
-          not $ TA.fromArray xs `TA.eq` (TA.fromArray ys :: TA.Int32Array)
+    describe "Int8Array" do
+      it "returns true when equal" $
+        quickCheck \xs ->
+          TA.fromArray xs `TA.eq` (TA.fromArray xs :: TA.Int8Array)
+      it "returns false when unequal" $
+        quickCheck \xs ->
+          arbitrary `suchThat` notEq xs <#> \ys ->
+            not $ TA.fromArray xs `TA.eq` (TA.fromArray ys :: TA.Int8Array)
+    describe "Int16Array" do
+      it "returns true when equal" $
+        quickCheck \xs ->
+          TA.fromArray xs `TA.eq` (TA.fromArray xs :: TA.Int16Array)
+      it "returns false when unequal" $
+        quickCheck \xs ->
+          arbitrary `suchThat` notEq xs <#> \ys ->
+            not $ TA.fromArray xs `TA.eq` (TA.fromArray ys :: TA.Int16Array)
+    describe "Int32Array" do
+      it "returns true when equal" $
+        quickCheck \xs ->
+          TA.fromArray xs `TA.eq` (TA.fromArray xs :: TA.Int32Array)
+      it "returns false when unequal" $
+        quickCheck \xs ->
+          arbitrary `suchThat` notEq xs <#> \ys ->
+            not $ TA.fromArray xs `TA.eq` (TA.fromArray ys :: TA.Int32Array)
   describe "notEq" $ do
-    it "returns false for equal Int32Arrays" $
-      quickCheck \xs ->
-        not $ TA.fromArray xs `TA.notEq` (TA.fromArray xs :: TA.Int32Array)
-    it "returns true for unequal Int32Arrays" $
-      quickCheck \xs ->
-        arbitrary `suchThat` notEq xs <#> \ys ->
-          TA.fromArray xs `TA.notEq` (TA.fromArray ys :: TA.Int32Array)
+    describe "Int8Array" do
+      it "returns false for equal" $
+        quickCheck \xs ->
+          not $ TA.fromArray xs `TA.notEq` (TA.fromArray xs :: TA.Int8Array)
+      it "returns true for unequal" $
+        quickCheck \xs ->
+          arbitrary `suchThat` notEq xs <#> \ys ->
+            TA.fromArray xs `TA.notEq` (TA.fromArray ys :: TA.Int8Array)
+    describe "Int16Array" do
+      it "returns false for equal" $
+        quickCheck \xs ->
+          not $ TA.fromArray xs `TA.notEq` (TA.fromArray xs :: TA.Int16Array)
+      it "returns true for unequal" $
+        quickCheck \xs ->
+          arbitrary `suchThat` notEq xs <#> \ys ->
+            TA.fromArray xs `TA.notEq` (TA.fromArray ys :: TA.Int16Array)
+    describe "Int32Array" do
+      it "returns false for equal" $
+        quickCheck \xs ->
+          not $ TA.fromArray xs `TA.notEq` (TA.fromArray xs :: TA.Int32Array)
+      it "returns true for unequal" $
+        quickCheck \xs ->
+          arbitrary `suchThat` notEq xs <#> \ys ->
+            TA.fromArray xs `TA.notEq` (TA.fromArray ys :: TA.Int32Array)
